18eb782876c7023f95e949d2d42b75d78274ec45
Leonardo Capristo <leonardo.capristo@hotmail.com>
2025-02-18 14:48:35 -0300

    first commit

---
+++ curso_programacao/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		int y = 32;
+		double x = 10.5345345;
+		
+		System.out.println("oi leo");
+		System.out.println(y);
+		System.out.println(x);
+		System.out.printf("%.2f", x);
+		
+		
+
+	}
+
+}
