4731411d5fd706f1ff681fde13a89366d8dd3409
Leonardo Capristo <leonardo.capristo@hotmail.com>
2025-09-29 10:07:19 -0300

    arrumando projeto

--- Recap/ExInterfaces1/src/model/services/OnlinePaymentService.java
+++
@@ -1,8 +0,0 @@
-package model.services;
-
-public interface OnlinePaymentService {
-    
-    Double paymentFee(Double amount);
-
-    Double interest(Double amount, Integer months);
-}
--- Recap/ExTratamantoExcecoes1/src/model/exceptions/IdadeInsuficienteException.java
+++
@@ -1,7 +0,0 @@
-package model.exceptions;
-
-public class IdadeInsuficienteException extends Exception {
-    public IdadeInsuficienteException() {
-        super("Você não tem idade suficiente para tirar a CNH. A idade mínima é 18 anos.");
-    }
-}
