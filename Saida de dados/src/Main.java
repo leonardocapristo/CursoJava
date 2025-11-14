b9f3f998acffeb8e086efd9a07da99c0e7d1f7f5
Leonardo Capristo <leonardo.capristo@hotmail.com>
2025-11-13 22:04:30 -0300

    Voltando

---
+++ Saida de dados/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Ola mundo");
+
+        double x = 1.34;
+
+        System.out.println(x);
+        System.out.printf("%.2f",x);
+        System.out.printf("");
+
+        /*
+        *  %f = ponto flutuante
+        *  %d = inteiro
+        *  %s = texto
+        *  %n = quebra de linha
+        *
+        *
+        *
+        * */
+
+
+        String nome = "Maria";
+        int idade = 22;
+        double renda = 400.00;
+
+        System.out.printf("%s tem %d anos e ganha %f", nome,idade,renda);
+        }
+}
