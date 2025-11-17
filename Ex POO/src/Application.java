8ed1ccae37a5fff3adc6abc4b0ec917c1b6a18b5
Leonardo Capristo <leonardo.capristo@hotmail.com>
2025-11-17 17:35:20 -0300

    ex poo

---
+++ Ex POO/src/Application.java
@@ -0,0 +1,41 @@
+import entities.Employee;
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Nome : ");
+        String nome = sc.nextLine();
+
+        System.out.println("Salário bruto : ");
+        double salarioBruto = sc.nextDouble();
+
+        System.out.println("Imposto : ");
+        double imposto = sc.nextDouble();
+
+        Employee employee = new Employee();
+
+        employee.name = nome;
+        employee.grossSalary = salarioBruto;
+        employee.tax = imposto;
+        double salarioLiquido = employee.netSalary();
+
+        System.out.println(employee.name+" "+ employee.netSalary());
+
+
+        System.out.println("Qual a porcentagem para aumentar o salário? ");
+        double aumento = sc.nextDouble();
+
+        employee.increaseSalary(aumento);
+
+        System.out.println("Dados atualizados : ");
+        System.out.println(employee.name+" "+ employee.netSalary());
+
+
+        sc.close();
+    }
+}
---
+++ Ex POO/src/entities/Employee.java
@@ -0,0 +1,28 @@
+package entities;
+
+public class Employee {
+
+    public String name;
+    public double grossSalary;
+    public double tax;
+
+
+    public double netSalary(){
+        double netSalary = grossSalary - tax;
+        return netSalary;
+    }
+
+    public void increaseSalary (double percentage){
+       grossSalary = grossSalary + (grossSalary * (percentage  / 100 ));
+
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", grossSalary=" + grossSalary +
+                ", tax=" + tax +
+                '}';
+    }
+}
