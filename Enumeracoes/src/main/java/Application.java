e2c5f5059f8808399953dc350cf15154f59bba0f
Leonardo Capristo <leonardo.capristo@hotmail.com>
2025-11-29 13:04:02 -0300

    att

--- Enumeracoes/src/main/java/Application.java
+++ Enumeracoes/src/main/java/Application.java
@@ -1,5 +1,20 @@
+import entities.Order;
+import enums.OrderStatus;
+
+
+import java.util.Date;
+
 public class Application {
     public static void main(String[] args) {
 
+
+        Order order = new Order(450, new Date(), OrderStatus.PENDING_PAYMENT);
+
+        System.out.println(order);
+
+
+        order.setStatus(OrderStatus.SHIPPED);
+
+        System.out.println(order);
     }
 }
--- Enumeracoes/src/main/java/entities/Order.java
+++ Enumeracoes/src/main/java/entities/Order.java
@@ -19,5 +19,36 @@ public class Order {
         this.status = status;
     }
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", moment=" + moment +
+                ", status=" + status +
+                '}';
+    }
 }
